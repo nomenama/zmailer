0aa13c7eca7ee60eeb07c2eedb6e98cece3f004c
nomen <justnomen@outlook.com>
2024-11-28 17:04:53 +0000

    handle all routes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/send-email", async (req, res) => {
 })
 
 app.use((req, res, next) => {
-	res.status(404).json({ error: "Route not found" });
+	res.status(200).json({ error: "Route not found" });
 });
 
 app.listen(PORT, () => writeLog(`Server started on port ${PORT}`));
